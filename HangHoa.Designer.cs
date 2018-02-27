22ceb71579e25bf30e03fd2fdbccedfc10938310
bichngoc97 <bichngoc97.vn@gmail.com>
2018-02-27 21:15:52 +0700

    formHangHoa

---
+++ HangHoa.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace QuanLyKho.GUI
+{
+    partial class HangHoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaHH = new System.Windows.Forms.TextBox();
+            this.txtTenHH = new System.Windows.Forms.TextBox();
+            this.txtMaNCC = new System.Windows.Forms.TextBox();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.dgvHangHoa = new System.Windows.Forms.DataGridView();
+            this.maHH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenHH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maNCC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soluong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHangHoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(598, 111);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hàng Hoá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã HH";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên HH";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mã NCC";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(312, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số Lượng";
+            // 
+            // txtMaHH
+            // 
+            this.txtMaHH.Enabled = false;
+            this.txtMaHH.Location = new System.Drawing.Point(92, 125);
+            this.txtMaHH.Name = "txtMaHH";
+            this.txtMaHH.Size = new System.Drawing.Size(138, 22);
+            this.txtMaHH.TabIndex = 5;
+            // 
+            // txtTenHH
+            // 
+            this.txtTenHH.Location = new System.Drawing.Point(92, 165);
+            this.txtTenHH.Name = "txtTenHH";
+            this.txtTenHH.Size = new System.Drawing.Size(138, 22);
+            this.txtTenHH.TabIndex = 6;
+            // 
+            // txtMaNCC
+            // 
+            this.txtMaNCC.Location = new System.Drawing.Point(382, 125);
+            this.txtMaNCC.Name = "txtMaNCC";
+            this.txtMaNCC.Size = new System.Drawing.Size(138, 22);
+            this.txtMaNCC.TabIndex = 7;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(382, 165);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(138, 22);
+            this.txtSoLuong.TabIndex = 8;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(16, 222);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 32);
+            this.btnThem.TabIndex = 9;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(133, 222);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 32);
+            this.btnSua.TabIndex = 10;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(262, 222);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 32);
+            this.btnXoa.TabIndex = 11;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(382, 222);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(75, 32);
+            this.btnLuu.TabIndex = 12;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // dgvHangHoa
+            // 
+            this.dgvHangHoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHangHoa.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.maHH,
+            this.tenHH,
+            this.maNCC,
+            this.soluong});
+            this.dgvHangHoa.Location = new System.Drawing.Point(4, 289);
+            this.dgvHangHoa.Name = "dgvHangHoa";
+            this.dgvHangHoa.RowTemplate.Height = 24;
+            this.dgvHangHoa.Size = new System.Drawing.Size(595, 196);
+            this.dgvHangHoa.TabIndex = 13;
+            this.dgvHangHoa.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHangHoa_CellContentClick);
+            // 
+            // maHH
+            // 
+            this.maHH.DataPropertyName = "maHH";
+            this.maHH.HeaderText = "Mã HH";
+            this.maHH.Name = "maHH";
+            // 
+            // tenHH
+            // 
+            this.tenHH.DataPropertyName = "tenHH";
+            this.tenHH.HeaderText = "Tên HH";
+            this.tenHH.Name = "tenHH";
+            // 
+            // maNCC
+            // 
+            this.maNCC.DataPropertyName = "maNCC";
+            this.maNCC.HeaderText = "Mã NCC";
+            this.maNCC.Name = "maNCC";
+            // 
+            // soluong
+            // 
+            this.soluong.DataPropertyName = "soluong";
+            this.soluong.HeaderText = "Số Lượng";
+            this.soluong.Name = "soluong";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(498, 222);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 32);
+            this.btnBack.TabIndex = 14;
+            this.btnBack.Text = "Làm Mới";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // HangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 485);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dgvHangHoa);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.txtMaNCC);
+            this.Controls.Add(this.txtTenHH);
+            this.Controls.Add(this.txtMaHH);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "HangHoa";
+            this.Text = "HangHoa";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHangHoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaHH;
+        private System.Windows.Forms.TextBox txtTenHH;
+        private System.Windows.Forms.TextBox txtMaNCC;
+        private System.Windows.Forms.TextBox txtSoLuong;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.DataGridView dgvHangHoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maHH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenHH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maNCC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soluong;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ HangHoa.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyKho.GUI
+{
+    public partial class HangHoa : Form
+    {
+        int a;
+        SqlCommand cmm;
+        string strConn = @"Data Source=NGOCXINH\SQLEXPRESS;Initial Catalog=QuanLyKho;Integrated Security=True";
+        SqlConnection conn = new SqlConnection();
+        private void LoadData()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * from HangHoa", conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgvHangHoa.DataSource = dt;
+        }
+        public HangHoa()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmm = new SqlCommand("select count(*) from HangHoa", conn);
+            SqlDataReader dr = cmm.ExecuteReader();
+            int soluong = 0;
+            if (dr.Read())
+            {
+                soluong = dr.GetInt32(0);
+                dr.Close();
+            }
+            for (int i = 0; i <= soluong; i++)
+            {
+                txtMaHH.Text = (i + 1).ToString();
+            }
+
+            btnLuu.Enabled = true;
+            btnSua.Enabled = false;
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+            a = 1;            
+
+            MessageBox.Show("Mời bạn thêm mới \n Ấn nút LƯU để lưu kết quả");
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            btnLuu.Enabled = true;
+            btnSua.Enabled = false;
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+            a = 2;
+            MessageBox.Show("Ấn nút LƯU để lưu kết quả");
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            btnLuu.Enabled = true;
+            btnSua.Enabled = false;
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+            a = 3;
+            MessageBox.Show("Ấn nút LƯU để lưu kết quả");
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+             switch(a){
+                case 1: 
+                    {
+                        cmm = new SqlCommand("select count(*) from HangHoa", conn);                        
+                   
+                        SqlCommand cmd = new SqlCommand("ThemHangHoa", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlParameter p = new SqlParameter();
+                        p = new SqlParameter("@MaHH", txtMaHH.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@TenHH", txtTenHH.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@MaNCC", txtMaNCC.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@SoLuong", txtSoLuong.Text);
+                        cmd.Parameters.Add(p);
+                        int count = cmd.ExecuteNonQuery();
+                        if (count > 0)
+                        {
+                            MessageBox.Show("Thêm mới thành công");
+                            LoadData();
+                        }
+                        else MessageBox.Show("Không thể thêm mới");
+                   
+                        btnLuu.Enabled = false;
+                        btnSua.Enabled = true;
+                        btnThem.Enabled = true;
+                        btnXoa.Enabled = true;
+                        break;
+                    }
+                case 2:
+                    {
+                        SqlCommand cmd = new SqlCommand("SuaHangHoa", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        SqlParameter p = new SqlParameter();
+                        p = new SqlParameter("@MaHH", txtMaHH.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@TenHH", txtTenHH.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@MaNCC", txtMaNCC.Text);
+                        cmd.Parameters.Add(p);
+                        p = new SqlParameter("@SoLuong", txtSoLuong.Text);
+                        cmd.Parameters.Add(p);
+                        int count = cmd.ExecuteNonQuery();
+                        if (count > 0)
+                        {
+                            MessageBox.Show("Sửa thành công");
+                            LoadData();
+                        }
+                        else MessageBox.Show("Không thể sửa");
+                        btnLuu.Enabled = false;
+                        btnSua.Enabled = true;
+                        btnThem.Enabled = true;
+                        btnXoa.Enabled = true;
+                        break;
+                    }
+                 case 3:
+                    {
+                        if (MessageBox.Show("Bạn có chắc chắn muốn xoá không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            SqlCommand cmd = new SqlCommand("XoaHangHoa", conn);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            SqlParameter p = new SqlParameter("@MaHH", txtMaHH.Text);
+                            cmd.Parameters.Add(p);
+
+                            int count = cmd.ExecuteNonQuery();
+                            if (count > 0)
+                            {
+                                MessageBox.Show("Xoá thành công!");
+                                LoadData();
+                                txtMaHH.Text = "";
+                                txtTenHH.Text = "";
+                                txtMaNCC.Text = "";
+                                txtSoLuong.Text = "";
+                            }
+                            else
+                            {
+                                MessageBox.Show("Không thể xoá bản ghi hiện tại!");
+                            }
+                        }
+                        btnLuu.Enabled = false;
+                        btnSua.Enabled = true;
+                        btnThem.Enabled = true;
+                        btnXoa.Enabled = true;
+                        break;
+                    }
+            }        
+        
+        }
+
+        private void dgvHangHoa_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                txtMaHH.Text = (dgvHangHoa.CurrentRow.Cells["maHH"].Value.ToString());
+                txtTenHH.Text = Convert.ToString(dgvHangHoa.CurrentRow.Cells["tenHH"].Value);
+                txtMaNCC.Text = Convert.ToString(dgvHangHoa.CurrentRow.Cells["maNCC"].Value);
+                txtSoLuong.Text = Convert.ToString(dgvHangHoa.CurrentRow.Cells["soluong"].Value);
+                
+            }         
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            LoadData();
+            txtMaHH.Text = "";
+            txtTenHH.Text = "";
+            txtMaNCC.Text = "";
+            txtSoLuong.Text = "";            
+            
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            btnXoa.Enabled = true;
+        }
+    }
+}
